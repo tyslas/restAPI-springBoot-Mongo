052f19d389fbce46f86d4275f23cff132c6d5e8e
Tito Yslas <roberto.yslas@gmail.com>
2018-01-16 13:34:34 -0700

    I have a RESTful API!

--- src/main/java/com/tito/restapifromscratch/PersonRepository.java
+++ src/main/java/com/tito/restapifromscratch/PersonRepository.java
@@ -1,4 +1,14 @@
 package com.tito.restapifromscratch;
 
-public class PersonRepository {
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends MongoRepository<Person, String> {
+
+  List<Person> findByLastName(@Param("name") String name);
+
 }
